13e0e83f09b634e268e26f386f5e8505a67fe6f6
Carl Chapman <carlallenchapman@gmail.com>
2015-03-07 19:53:24 -0600

    this repo is complete at this point, but has not been reviewed by peers.  So every challenge and corresponding test suite is ready to be used, and has been successfully passed using the right rejex.

---
+++ src/compose/match/ValidPhoneNumber.java
@@ -0,0 +1,24 @@
+package compose.match;
+
+import java.util.regex.Pattern;
+
+public class ValidPhoneNumber {
+
+	/**
+	 * A line of text will contain at most one newline and only then at the end
+	 * of the string (this input will not have multiple lines).
+	 * 
+	 * This function should take one line of text and verify that the entire
+	 * string is composed of one valid phone number. Extra characters like
+	 * whitespace before or after, or anything that would invalidate the phone
+	 * number are not allowed (except newline at the end). For our purposes, we
+	 * only accept phone numbers with dashes like: ###-###-####
+	 * 
+	 */
+	public boolean isValidPhoneNumber(String line) {
+
+		// TODO compose a regex to complete the challenge
+		String regex = "";
+		return Pattern.matches(regex, line);
+	}
+}
