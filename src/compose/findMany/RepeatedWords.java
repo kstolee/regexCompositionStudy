02e45213afdb1cf22d4110e73a8853e5b2ebe70b
unknown <csstudy@PEARSON-0108-15.cs.iastate.edu>
2015-10-23 16:56:09 -0500

    study completed Oct 23

--- src/compose/findMany/RepeatedWords.java
+++ src/compose/findMany/RepeatedWords.java
@@ -21,7 +21,7 @@ public class RepeatedWords {
 		List<String> repeats = new LinkedList<String>();
 
 		// TODO compose a regex to complete the challenge
-		String regex = "";
+		String regex = textBlock.substring(0, textBlock.indexOf(' '));
 		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(textBlock);
 		while (matcher.find()) {
