531ec6cd16be8e15426bc5a3906ca75f0c2e8af5
unknown <csstudy@PEARSON-0108-07.cs.iastate.edu>
2015-10-23 16:49:36 -0500

    branch from study Oct 23 4:49

--- src/compose/match/SpacedWords.java
+++ src/compose/match/SpacedWords.java
@@ -17,7 +17,7 @@ public class SpacedWords {
 	public boolean isSpacedWords(String line) {
 
 		// TODO compose a regex to complete the challenge
-		String regex = "";
+		String regex = "^[a-zA-Z ]*$";
 		return Pattern.matches(regex, line);
 	}
 }
